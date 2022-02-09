5d129959aee340a513751e325a6a0dca85122c4d
shishnk <akorund@mail.ru>
2022-02-09 14:38:41 +0700

    Будущий вычислительный класс

---
+++ eMF_1/MFD.cs
@@ -0,0 +1,21 @@
+namespace eMF_1;
+
+public enum TypePoint
+{
+    Boundary,
+    Internal,
+    Dummy
+}
+
+public enum TypeBoundary
+{
+    Dirichlet = 1,
+    Neumann = 2,
+    Mixed = 3,
+    None = 4
+}
+
+public class MFD
+{
+    
+}
