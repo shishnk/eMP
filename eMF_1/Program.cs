c1810b677be1d2dc3a68f5c86ebd420f27088c40
shishnk <akorund@mail.ru>
2022-02-14 23:16:39 +0700

    Клиентский код

--- eMF_1/Program.cs
+++ eMF_1/Program.cs
@@ -1,3 +1,8 @@
 ﻿using eMF_1;
 
-// MFD mfd = new();
+GridFactory gridFactory = new();
+// MFD mfd = new(gridFactory.CreateGrid(GridType.Irregular, "grid/grid(irregular).txt"));
+MFD mfd = new(gridFactory.CreateGrid(GridType.Regular, "grid/grid(regular).txt"));
+mfd.SetTest(new FirstTest());
+mfd.SetMethodSolvingSLAE(new GaussSeidel(1000, 1E-14));
+mfd.Compute();
