75f400c7be4ba0e1a476a835b40d2f2ea8be6063
shishnk <akorund@mail.ru>
2022-02-14 23:17:49 +0700

    Модуль тестов

---
+++ eMF_1/FirstTest.cs
@@ -0,0 +1,9 @@
+namespace eMF_1;
+
+    public class FirstTest : ITest
+    {
+        public double F(Point2D point)
+            => 0;
+        public double U(Point2D point)
+            => 0;
+    }
---
+++ eMF_1/ITest.cs
@@ -0,0 +1,7 @@
+namespace eMF_1;
+
+public interface ITest
+{
+    public double U(Point2D point);
+    public double F(Point2D point);
+}
