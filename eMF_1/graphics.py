3ebc669742e51f2f931350df7080e585d9e203dc
shishnk <akorund@mail.ru>
2022-02-14 23:20:16 +0700

    Добавил отображение сетки

--- eMF_1/graphics.py
+++ eMF_1/graphics.py
@@ -2,23 +2,25 @@ import matplotlib.pyplot as plt
 
 xB, xI, xD, yB, yI, yD = [], [], [], [], [], []
 
-with open("boundaryPoints.txt") as file:
+with open("points/boundaryPoints.txt") as file:
     for line in file:
         xC, yC = line.split()
         xB.append(float(xC))
         yB.append(float(yC))
 
-with open("internalPoints.txt") as file:
+with open("points/internalPoints.txt") as file:
     for line in file:
         xC, yC = line.split()
         xI.append(float(xC))
         yI.append(float(yC))
 
-with open("dummyPoints.txt") as file:
+with open("points/dummyPoints.txt") as file:
     for line in file:
         xC, yC = line.split()
         xD.append(float(xC))
         yD.append(float(yC))
+
+plt.grid()
 
 plt.plot(xB, yB, 'o', color='r')
 plt.plot(xI, yI, 'o', color='y')
