8dc5a5d863e004c1ebddb9982844a37af2d667b8
shishnk <akorund@mail.ru>
2022-02-17 00:24:26 +0700

    Убрал record, добавил индексы i, j

--- eMF_1/Point2D.cs
+++ eMF_1/Point2D.cs
@@ -1,25 +1,28 @@
 namespace eMF_1;
 
-public record struct Point2D
+public struct Point2D
 {
     public double X { get; init; }
     public double Y { get; init; }
+    public int I { get; init; }
+    public int J { get; init; }
     public PointType PointType { get; set; }
     public BoundaryType BoundaryType { get; set; } = BoundaryType.Dirichlet;
 
-    public Point2D(double x, double y, PointType pointType)
+    public Point2D(double x, double y, int i, int j, PointType pointType)
     {
         X = x;
         Y = y;
+        I = i;
+        J = j;
         PointType = pointType;
-        BoundaryType = BoundaryType.Dirichlet;
     }
 
     public static Point2D Parse(string pointStr)
     {
         var data = pointStr.Split();
         Point2D point = new(double.Parse(data[0]), double.Parse(data[1]),
-        (PointType)Enum.Parse(typeof(PointType), data[2]));
+        int.Parse(data[2]), int.Parse(data[3]), (PointType)Enum.Parse(typeof(PointType), data[4]));
 
         return point;
     }
