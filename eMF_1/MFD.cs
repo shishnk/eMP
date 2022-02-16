9ca27ee7ccb834a54505a587c4a3bf370d8bff28
shishnk <akorund@mail.ru>
2022-02-17 00:21:08 +0700

    Добавить краевые условия и разобраться с ошибкой подсчета

--- eMF_1/MFD.cs
+++ eMF_1/MFD.cs
@@ -24,12 +24,31 @@ public enum GridType
 public class MFD
 {
     private Grid _grid;
+    private DiagMatrix _matrix;
     private ITest _test;
     private ISolver _solver;
+    private Boundary[] _boundaries;
+    private double[] _q;
+    private double[] _pr;
+    public double[] Weights
+        => _q;
 
-    public MFD(Grid grid)
+    public MFD(Grid grid, string boundaryPath)
     {
-        _grid = grid;
+        try
+        {
+            using (var sr = new StreamReader(boundaryPath))
+            {
+                _boundaries = sr.ReadToEnd().Split("\n")
+                .Select(str => Boundary.BoundaryParse(str)).ToArray();
+            }
+
+            _grid = grid;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 
     public void SetTest(ITest test)
@@ -49,11 +68,60 @@ public class MFD
                 throw new Exception("Set the method solving SLAE!");
 
             _grid.Build();
+            Init();
+            BuildMatrix();
+            _q = _solver.Compute(_matrix, _pr);
 
         }
         catch (Exception ex)
         {
             Console.WriteLine(ex.Message);
+        }
+    }
+
+    private void Init()
+    {
+        _matrix = new(_grid.Points.Count, _grid.LinesX.Length - 1);
+        _pr = new double[_matrix.Size];
+        _q = new double[_matrix.Size];
+    }
+
+    private void BuildMatrix() // TODO (разобраться с проблемой подсчета внутреннего узла, добавить краевые условия)
+    {
+        for (int i = 0; i < _grid.Points.Count; i++)
+        {
+            switch (_grid.Points[i].PointType)
+            {
+                case PointType.Boundary:
+
+                    _matrix.Diags[0][i] = 1;
+                    _pr[i] = _test.U(_grid.Points[i]);
+
+                    break;
+
+                case PointType.Internal:
+
+                    double hx = _grid.AllLinesX[_grid.Points[i].I + 1] - _grid.AllLinesX[_grid.Points[i].I];
+                    double hy = _grid.AllLinesY[_grid.Points[i].J + 1] - _grid.AllLinesY[_grid.Points[i].J];
+                    _pr[i] = _test.F(_grid.Points[i]);
+                    _matrix.Diags[0][i] = -(2.0 / (hx * hx) + 2.0 / (hy * hy));
+                    _matrix.Diags[1][i - 1] = 1.0 / (hx * hx);
+                    _matrix.Diags[3][i] = 1.0 / (hx * hx);
+                    _matrix.Diags[2][i - 3] = 1.0 / (hy * hy);
+                    _matrix.Diags[4][i] = 1.0 / (hy * hy);
+
+                    break;
+
+                case PointType.Dummy:
+
+                    _matrix.Diags[0][i] = 1;
+                    _pr[i] = 0;
+
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 }
