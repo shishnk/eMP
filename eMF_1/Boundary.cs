ab110b46180859bc6e5b950054cfc6427eb8d2a8
shishnk <akorund@mail.ru>
2022-02-17 00:22:44 +0700

    Структура для краевого

---
+++ eMF_1/Boundary.cs
@@ -0,0 +1,13 @@
+namespace eMF_1;
+
+public readonly record struct Boundary(BoundaryType BoundaryType, int X1, int X2, int Y1, int Y2)
+{
+    public static Boundary BoundaryParse(string boundaryStr)
+    {
+        var data = boundaryStr.Split();
+        Boundary boundary = new((BoundaryType)Enum.Parse(typeof(BoundaryType), data[0]), 
+        int.Parse(data[1]), int.Parse(data[2]), int.Parse(data[3]), int.Parse(data[4]));
+
+        return boundary;
+    }
+}
