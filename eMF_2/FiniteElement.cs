cc1aa74002b5e454e9164f4725b5bb51fc790020
shishnk <akorund@mail.ru>
2022-02-25 20:59:35 +0700

    Структура для конечного элемента

---
+++ eMF_2/FiniteElement.cs
@@ -0,0 +1,26 @@
+namespace eMF_2;
+
+public record struct Element
+{
+    public double LeftBorder { get; init; }
+    public double RightBorder { get; init; }
+    public double Lenght { get; init; }
+
+    public Element(double leftBorder, double rightBorder)
+    {
+        LeftBorder = leftBorder;
+        RightBorder = rightBorder;
+        Lenght = Math.Abs(rightBorder - leftBorder);
+    }
+
+    public static Element Parse(string str)
+    {
+        var data = str.Split();
+        Element element = new(double.Parse(data[0]), double.Parse(data[1]));
+
+        return element;
+    }
+
+    public override string ToString()
+        => $"Element interval is [{LeftBorder}; {RightBorder}]";
+}
