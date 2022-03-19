22af28680146621b539fa42d8f9da3ddcfd17273
Breadp4ck <iii103@mail.ru>
2022-03-19 13:03:43 +0700

    Что-то начал делать

--- eMF_2/Basis.cs
+++ eMF_2/Basis.cs
@@ -1,7 +1,9 @@
 namespace eMF_2;
 
-// $x = \xi = \dfrac{x(точка) - x_{2k-1}} / h_k$,
-// где $h_k = x_{2k+1} - x_{2k-1}$ (длина конечного элемента) 
+/// <summary>
+/// $x = \xi = \dfrac{x(точка) - x_{2k-1}} / h_k$,
+/// где $h_k = x_{2k+1} - x_{2k-1}$ (длина конечного элемента) 
+/// </summary>
 public static class Basis {
     public static double Psi1(double x)
         => 2 * (x - 0.5) * (x - 1);
---
+++ eMF_2/Matrix.cs
@@ -0,0 +1,44 @@
+namespace eMF_2;
+
+interface IMatrix { }
+
+/// <summary>
+/// Класс <c>TapeMatrix</c> используется для создания матрицы
+/// с ленточным форматом хранения элементов.
+/// </summary>
+public class TapeMatrix : IMatrix {
+    public double[][] RawData { get; private set; }
+    public uint Dimension { get; init; }
+    public uint Lines { get; init; }
+
+    /// <summary>
+    /// Конструктор в качестве аргументов принимает размерности матрицы <c>dimension</c> и
+    /// количество лент <c>lines</c>, которое должно быть нечётным числом.
+    /// Матрица хранится по строкам.
+    /// </summary>
+    public TapeMatrix(uint dimension, uint lines) {
+        if (lines % 2 == 0) {
+            throw "Число диагоналей должно быть нечётным.";
+        }
+
+        RawData = new double[dimensions][lines];
+        Dimension = dimension;
+        Lines = lines;
+    }
+
+    /// <summary>
+    /// Метод <c>ByIndex</c> возвращает элемент матрицы по индексам как если бы она была плотной.
+    /// Первый аргумент <c>i</c> - строка, второй <c>j</c> - столбец, индексация с нуля.
+    /// </summary>
+    public async double ByIndex(uint i, uint j) {
+        if (i > Dimension || j > Dimension) {
+            throw "У матрицы нет элемента с такими индексами.";
+        }
+
+        if (Abs(i - j) > Lines / 2) {
+            return 0.0;
+        }
+
+        return RawData[i][j - i + 1];
+    }
+}
