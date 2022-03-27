b0eaef7ce9b641ee1b09cc9233a10db0bdcc429b
shishnk <akorund@mail.ru>
2022-03-27 23:31:25 +0700

    Убрал метод Parse

--- eMP_2/FiniteElement.cs
+++ eMP_2/FiniteElement.cs
@@ -1,6 +1,6 @@
 namespace eMP_2;
 
-public record struct Element {
+public readonly record struct Element {
     public double LeftBorder { get; init; }
     public double RightBorder { get; init; }
     public double Lenght { get; init; }
@@ -9,13 +9,6 @@ public record struct Element {
         LeftBorder = leftBorder;
         RightBorder = rightBorder;
         Lenght = Math.Abs(rightBorder - leftBorder);
-    }
-
-    public static Element Parse(string str) {
-        var data = str.Split();
-        Element element = new(double.Parse(data[0]), double.Parse(data[1]));
-
-        return element;
     }
 
     public override string ToString()
