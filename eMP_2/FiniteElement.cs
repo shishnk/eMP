fef2302a7a665c2a24facd62a6da19bc8675c719
shishnk <akorund@mail.ru>
2022-03-28 00:04:53 +0700

    Изменил реализацию

--- eMP_2/FiniteElement.cs
+++ eMP_2/FiniteElement.cs
@@ -1,16 +1,11 @@
 namespace eMP_2;
 
-public readonly record struct Element {
-    public double LeftBorder { get; init; }
-    public double RightBorder { get; init; }
-    public double Lenght { get; init; }
+public class FiniteElement {
+    public Interval Interval { get; init; }
 
-    public Element(double leftBorder, double rightBorder) {
-        LeftBorder = leftBorder;
-        RightBorder = rightBorder;
-        Lenght = Math.Abs(rightBorder - leftBorder);
-    }
+    public FiniteElement(Interval interval)
+        => Interval = interval;
 
     public override string ToString()
-        => $"Element interval is [{LeftBorder}; {RightBorder}]";
+        => $"Element interval is [{Interval.LeftBorder}; {Interval.RightBorder}]";
 }
