687d13f210cb080f597edfb08d931ab1c7181065
shishnk <akorund@mail.ru>
2022-03-27 23:15:56 +0700

    Тесты будут храниться в одном файле

--- eMP_2/Tests/ITest.cs
+++ eMP_2/Tests/ITest.cs
@@ -1,7 +1,15 @@
 namespace eMP_2;
 
 public interface ITest {
-    public double U(Point2D point);
+    public double U(double point);
 
-    public double F(Point2D point);
+    public double F(double point);
+}
+
+public class FirstTest : ITest {
+    public double U(double point)
+        => point;
+
+    public double F(double point)
+        => 0;
 }
