bacdc9d066568ca6a3c6b58952d5304505e80447
shishnk <akorund@mail.ru>
2022-03-27 23:19:07 +0700

    Клиентский код (TODO -> почистить от чтения файлов)

---
+++ eMP_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using eMP_2;
+
+GridParameters? timeGridParameters;
+GridParameters? spaceGridParameters;
+
+try {
+    var sr = new StreamReader("timeGrid.json");
+    using (sr) {
+        timeGridParameters = JsonConvert.DeserializeObject<GridParameters>(sr.ReadToEnd());
+    }
+    sr = new StreamReader("spaceGrid.json");
+    using (sr) {
+        spaceGridParameters = JsonConvert.DeserializeObject<GridParameters>(sr.ReadToEnd());
+    }
+
+    GridFactorySpace gridFactorySpace = new();
+    GridFactoryTime gridFactoryTime = new();
+    var grid1 =gridFactorySpace.CreateGrid(GridType.SpaceIrregular, spaceGridParameters!);
+    var grid2 = gridFactoryTime.CreateGrid(GridType.TimeIrregular, timeGridParameters!);
+    
+} catch (Exception ex) {
+    Console.WriteLine(ex.Message);
+}
+
+Console.WriteLine();
