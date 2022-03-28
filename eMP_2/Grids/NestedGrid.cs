df834af2e0ebfb03c990bd592db4c466a22a9508
shishnk <akorund@mail.ru>
2022-03-28 18:33:12 +0700

    Добавил формирование внутренних узлов для базиса

--- eMP_2/Grids/NestedGrid.cs
+++ eMP_2/Grids/NestedGrid.cs
@@ -13,9 +13,12 @@ public abstract class NestedGrid : IGrid {
             if (TimeDependent == true && gridParameters.Interval.LeftBorder < 0)
                 throw new Exception("The beginning of the time segment cannot be less than 0");
 
+            if (gridParameters.Splits < 1)
+                throw new Exception("The number of splits must be greater than or equal to 1");
+
             ArgumentNullException.ThrowIfNull(gridParameters.K, $"{nameof(gridParameters.K)} cannot be null");
 
-            _points = new double[gridParameters.Splits + 1];
+            _points = new double[2 * gridParameters.Splits + 1];
             double h;
             double sum = 0;
             double coef = Math.Sqrt((double)gridParameters.K);
@@ -27,13 +30,17 @@ public abstract class NestedGrid : IGrid {
             _points[0] = gridParameters.Interval.LeftBorder;
             _points[^1] = gridParameters.Interval.RightBorder;
 
-            int index = 1;
+            int index = 2;
 
             while (_points[index] != gridParameters.Interval.RightBorder) {
-                _points[index] = _points[index - 1] + h;
+                _points[index] = _points[index - 2] + h;
                 h *= (double)gridParameters.K;
                 index++;
             }
+
+            for (int i = 1; i < _points.Length; i++)
+                if (_points[i] == 0)
+                    _points[i] = (_points[i + 1] + _points[i - 1]) / 2;
 
         } catch (Exception ex) {
             Console.WriteLine($"We had problem: {ex.Message}");
