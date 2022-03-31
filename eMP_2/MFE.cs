4ada458d5ab8d4cff5e034d8d4c33b7ba75785d9
shishnk <akorund@mail.ru>
2022-03-31 17:41:13 +0700

    Добавлена локальная сборка матриц и вектора (TODO-> глобальная сборка)

--- eMP_2/MFE.cs
+++ eMP_2/MFE.cs
@@ -33,18 +33,20 @@ public class MFE {
     // default! указывает на то, что данное поле не может принимать null
     public delegate double Basis(double x);
 
-    private Integration _integration = new();
+    private readonly Integration _integration = new();
     private Basis[] _basis = default!;
     private Basis[] _dBasis = default!;
     private ITest _test = default!;
     private ISolver _solver = default!;
     private IGrid _spaceGrid = default!;
     private IGrid _timeGrid = default!;
-    private Matrix M = default!; // матрица масс
-    private Matrix? G; // матрица жесткости
+    private Matrix? _massMatrix; // матрица масс
+    private Matrix _stiffnessMatrix = default!; // матрица жесткости
+    private DiagMatrix _globalMatrix = default!;
     private FiniteElement[] _elements = default!;
-    private double[] q = default!; // вектор весов
-    private double[] f = default!; // вектор правой части
+    private double[] _weights = default!; // вектор весов
+    private double[] _localVector = default!;
+    private double[] _vector = default!; // вектор правой части
 
     public void Compute() {
         try { // не забыть раскомментировать!
@@ -52,44 +54,75 @@ public class MFE {
             // ArgumentNullException.ThrowIfNull(_solver, $"{nameof(_solver)} cannot be null, set the method of solving SLAE");
 
             Init();
-            Console.WriteLine();
+            AssemblyLocalVector(2);
+            AssemblyGlobalMatrix();
+            AssemblyGlobalVector();
 
 
         } catch (Exception ex) {
-            Console.WriteLine(ex.Message);
+            Console.WriteLine($"We had problem: {ex.Message}");
         }
     }
 
     private void Init() {
-        M = new(3);
-        G = new(3);
+        _massMatrix = (_spaceGrid.Sigma is not null) ? new(3) : null;
+        _stiffnessMatrix = new(3);
+        _globalMatrix = new(_spaceGrid.Points.Length);
+        _vector = new double[7];
+        _localVector = new double[3];
         _basis = new Basis[] { QuadraticBasis.Psi1, QuadraticBasis.Psi2, QuadraticBasis.Psi3 };
         _dBasis = new Basis[] { QuadraticBasis.DPsi1, QuadraticBasis.DPsi2, QuadraticBasis.DPsi3 };
-        _integration = new();
         _elements = new FiniteElement[_spaceGrid.Points.Length - 1];
 
-        int index = 0;
-
-        for (int ielem = 0; ielem < _elements.Length; ielem++) { // формирование конечных элементов
+        for (int ielem = 0, index = 0; ielem < _elements.Length; ielem++, index++) // формирование конечных элементов
             _elements[ielem] = new(new(_spaceGrid.Points[index], _spaceGrid.Points[index + 1]));
-            index++;
+    }
+
+    private void AssemblyLocalMatrices(int ielem) {
+        for (int i = 0; i < _stiffnessMatrix.Size; i++)
+            for (int j = 0; j < _stiffnessMatrix.Size; j++) {
+                _stiffnessMatrix[i, j] = _integration.GaussOrder5(_dBasis[i], _dBasis[j], 0, 1) / _elements[ielem].Interval.Lenght;
+
+                if (_massMatrix is not null)
+                    _massMatrix[i, j] = _integration.GaussOrder5(_basis[i], _basis[j], 0, 1) * _elements[ielem].Interval.Lenght;
+            }
+
+        if (_massMatrix is not null)
+            _stiffnessMatrix += _massMatrix;
+    }
+
+    private void AssemblyGlobalMatrix() {
+        for (int ielem = 0; ielem < _elements.Length; ielem++) {
+            AssemblyLocalMatrices(ielem);
+
+            _stiffnessMatrix.Clear();
+            _massMatrix?.Clear();
         }
-
-        // for (int i = 0; i < M.Size; i++) // рабочий алгоритм взятия интегралов от базисных функций на шаблонном элементе
-        //     for (int j = 0; j < M.Size; j++)
-        //         M[i, j] = _integration.GaussOrder5(_dBasis[i], _dBasis[j]) / _elements[0].Interval.Lenght;
     }
 
-    private void BuildLocalMatrix() {
+    private void AssemblyGlobalVector() {
+        for (int ielem = 0; ielem < _elements.Length; ielem++) {
 
+
+            Array.Clear(_localVector);
+        }
     }
 
-    private void BuildGlobalMatrix() {
+    private void AssemblyLocalVector(int ielem) {
+        double[] elementPoints = new double[3];
 
-    }
+        elementPoints[0] = _elements[ielem].Interval.LeftBorder;
+        elementPoints[1] = _elements[ielem].Interval.Center;
+        elementPoints[2] = _elements[ielem].Interval.RightBorder;
 
-    private void BuildF() {
+        for (int i = 0; i < _localVector.Length; i++)
+            _localVector[i] = _elements[ielem].Interval.Lenght * _test.F(elementPoints[i]) * _integration.GaussOrder5(_basis[i], 0, 1);
 
+        /* заполнение локального вектора возможно через матрицу масс(разложения компонентов на линейный интерполянт), 
+        но если в программе задать коэффициент перед ней нуль, то придется инициализировать ручками компоненты матрицы масс
+         for (int i = 0; i < _massMatrix?.Size; i++)
+             for (int j = 0; j < _massMatrix?.Size; j++)
+                 _localVector[i] += _elements[ielem].Interval.Lenght * _test.F(elementPoints[j]) * _massMatrix[i, j]; */
     }
 
     public static MFEBuilder CreateBuilder()
