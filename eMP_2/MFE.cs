cb626ac21c43f9d3da5b41eded6c736bc6fea656
shishnk <akorund@mail.ru>
2022-04-03 03:31:21 +0700

    Написал сборку глобальной матрицы для другого формата ленточной матрицы (TODO-> проверить точно ли корректно работает сборка)

--- eMP_2/MFE.cs
+++ eMP_2/MFE.cs
@@ -43,24 +43,26 @@ public class MFE {
     private Matrix? _massMatrix; // матрица масс
     private Matrix _stiffnessMatrix = default!; // матрица жесткости
     private DiagMatrix _globalMatrix = default!;
+    private BandMatrix _remasteredGlobalMatrix = default!;
     private FiniteElement[] _elements = default!;
     private double[] _localVector = default!;
     private double[] _vector = default!; // вектор правой части
 
     public void Compute() {
         try {
-            ArgumentNullException.ThrowIfNull(_test, $"{nameof(_test)} cannot be null, set the test"); 
+            ArgumentNullException.ThrowIfNull(_test, $"{nameof(_test)} cannot be null, set the test");
             ArgumentNullException.ThrowIfNull(_solver, $"{nameof(_solver)} cannot be null, set the method of solving SLAE");
 
             Init();
             AssemblyGlobalMatrix();
+            AssemblyRemasteredGlobalMatrix();
             AssemblyGlobalVector();
             _solver.SetMatrix(_globalMatrix);
             _solver.SetVector(_vector);
             // _solver.Compute();
 
             // foreach (var weight in _solver.Solution!)
-                // Console.WriteLine(weight);
+            // Console.WriteLine(weight);
 
         } catch (Exception ex) {
             Console.WriteLine($"We had problem: {ex.Message}");
@@ -71,6 +73,7 @@ public class MFE {
         _massMatrix = (_spaceGrid.Sigma.HasValue && _spaceGrid.Sigma != 0) ? new(3) : null;
         _stiffnessMatrix = new(3);
         _globalMatrix = new(2 * _spaceGrid.Points.Length - 1);
+        _remasteredGlobalMatrix = new(2 * _spaceGrid.Points.Length - 1, 2);
         _vector = new double[_globalMatrix.Size];
         _localVector = new double[3];
         _basis = new Basis[] { QuadraticBasis.Psi1, QuadraticBasis.Psi2, QuadraticBasis.Psi3 };
@@ -114,6 +117,41 @@ public class MFE {
             _globalMatrix.Diags[3][index] += _stiffnessMatrix[0, 1];
             _globalMatrix.Diags[3][index + 1] += _stiffnessMatrix[1, 2];
             _globalMatrix.Diags[4][index] += _stiffnessMatrix[0, 2];
+
+            _stiffnessMatrix.Clear();
+            _massMatrix?.Clear();
+        }
+    }
+
+    private void AssemblyRemasteredGlobalMatrix() {
+        for (int ielem = 0; ielem < _elements.Length; ielem++) {
+            AssemblyLocalMatrices(ielem);
+
+            int index = 2 * ielem;
+
+            for (int i = 0; i < _stiffnessMatrix.Size; i++)
+                _remasteredGlobalMatrix.Diagonal[index + i] += _stiffnessMatrix[i, i];
+
+            for (int i = 0; i < _stiffnessMatrix.Size - 1; i++)
+                for (int j = i + 1; j < _stiffnessMatrix.Size; j++) {
+                    int tmpi = i;
+                    int tmpj = j;
+
+                    if (j > 1)
+                        (tmpi, tmpj) = (j - 1, i);
+
+                    _remasteredGlobalMatrix.Lower[index + tmpi + 1][tmpj] += _stiffnessMatrix[j, i];
+                }
+
+            for (int i = 0; i < _stiffnessMatrix.Size - 1; i++)
+                for (int j = i + 1; j < _stiffnessMatrix.Size; j++) {
+                    int tmpj = j;
+
+                    if (i >= 1)
+                        tmpj--;
+
+                    _remasteredGlobalMatrix.Upper[index + i][tmpj - 1] += _stiffnessMatrix[i, j];
+                }
 
             _stiffnessMatrix.Clear();
             _massMatrix?.Clear();
