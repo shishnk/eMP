492c738dfaa40ef480a1ece2a41e6c132c2cfa54
shishnk <akorund@mail.ru>
2022-04-19 23:32:26 +0700

    Добавил метод умножения матрицы на вектор

--- eMP_2/BandMatrix.cs
+++ eMP_2/BandMatrix.cs
@@ -14,4 +14,38 @@ public class BandMatrix {
         Upper = new double[size].Select(column => new double[columnCount]).ToArray();
         Lower = new double[size].Select(column => new double[columnCount]).ToArray();
     }
+
+    public void Clear() {
+        for (int i = 0; i < Size; i++) {
+            Diagonal[i] = 0;
+
+            for (int j = 0; j < ColumnCount; j++) {
+                if (i + j < ColumnCount)
+                    continue;
+
+                Lower[i][j] = 0;
+                Upper[i][j] = 0;
+            }
+        }
+    }
+
+    public static double[] operator *(BandMatrix matrix, double[] vector) {
+        double[] result = new double[matrix.Size];
+
+        for (int i = 0; i < matrix.Size; i++) {
+            result[i] = matrix.Diagonal[i] * vector[i];
+
+            for (int j = 0; j < matrix.ColumnCount; j++) {
+                if (i + j < matrix.ColumnCount)
+                    continue;
+
+                int index = Math.Abs(matrix.ColumnCount - i - j);
+
+                result[i] += matrix.Lower[i][j] * vector[index];
+                result[index] += matrix.Upper[i][j] * vector[i];
+            }
+        }
+
+        return result;
+    }
 }
