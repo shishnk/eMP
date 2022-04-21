13f9763e1b6f753c68ff2666ba06dc1879198f12
shishnk <akorund@mail.ru>
2022-04-21 21:31:06 +0700

    Добавил параметром время

--- eMP_2/Tests/Tests.cs
+++ eMP_2/Tests/Tests.cs
@@ -1,15 +1,15 @@
 namespace eMP_2;
 
 public interface ITest {
-    public double U(double point);
+    public double U(double point, double t);
 
-    public double F(double point);
+    public double F(double point, double t);
 }
 
 public class Test1 : ITest {
-    public double U(double point)
-        => point * point;
+    public double U(double point, double t)
+        => point * point + t;
 
-    public double F(double point)
-        => -5 * point * point;
+    public double F(double point, double t)
+        => -6 * point * point - 2 * t + 1;
 }
