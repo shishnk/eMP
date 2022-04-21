b46f03258ef8b8552f9ca090bad9ecde1030bb35
shishnk <akorund@mail.ru>
2022-04-22 00:05:35 +0700

    Клиентский код (внес поправки)

--- eMP_2/Program.cs
+++ eMP_2/Program.cs
@@ -9,5 +9,6 @@ GridFactoryTime gridFactoryTime = new();
 IGrid spaceGrid = gridFactorySpace.CreateGrid(GridTypes.SpaceRegular, spaceGridParameters!);
 IGrid timeGrid = gridFactoryTime.CreateGrid(GridTypes.TimeRegular, timeGridParameters!);
 
-MFE mfe = MFE.CreateBuilder().SetSpaceGrid(spaceGrid).SetTimeGrid(timeGrid).SetTest(new FirstTest()).SetMethod(new DecomposerLU());
+MFE mfe = MFE.CreateBuilder().SetSpaceGrid(spaceGrid).SetTimeGrid(timeGrid).SetTest(new Test1())
+.SetDecomposer(new DecomposerLU()).SetMethod((NonLinearSolverTypes.SimpleIteration, 1000, 1E-14));
 mfe.Compute();
