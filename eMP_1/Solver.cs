f507da7cc6729de947ed8d66352c404690b9b442
shishnk <akorund@mail.ru>
2022-04-29 01:40:35 +0700

    Поместил решатель и интерфейс в один файл

--- eMP_1/Solver.cs
+++ eMP_1/Solver.cs
@@ -1,18 +1,22 @@
 namespace eMF_1;
 
-public record GaussSeidel(int MaxIters, double Eps, double W) : ISolver
-{
-    public double[] Compute(DiagMatrix diagMatrix, double[] pr)
-    {
+public interface ISolver {
+    public int MaxIters { get; init; }
+    public double Eps { get; init; }
+    public double W { get; init; }
+
+    public double[] Compute(DiagMatrix diagMatrix, double[] pr);
+}
+
+public record GaussSeidel(int MaxIters, double Eps, double W) : ISolver {
+    public double[] Compute(DiagMatrix diagMatrix, double[] pr) {
         double[] qk = new double[diagMatrix.Size];
         double[] qk1 = new double[diagMatrix.Size];
         double[] residual = new double[diagMatrix.Size];
         double prNorm = pr.Norm();
 
-        for (int i = 0; i < MaxIters; i++)
-        {
-            for (int k = 0; k < diagMatrix.Size; k++)
-            {
+        for (int i = 0; i < MaxIters; i++) {
+            for (int k = 0; k < diagMatrix.Size; k++) {
                 double fstSum = MultLine(diagMatrix, k, qk1, 1);
                 double scdSum = MultLine(diagMatrix, k, qk, 2);
 
@@ -30,14 +34,11 @@ public record GaussSeidel(int MaxIters, double Eps, double W) : ISolver
         return qk;
     }
 
-    private double MultLine(DiagMatrix diagMatrix, int i, double[] vector, int method)
-    {
+    private static double MultLine(DiagMatrix diagMatrix, int i, double[] vector, int method) {
         double sum = 0;
 
-        if (method == 0 || method == 1)
-        {
-            if (i > 0)
-            {
+        if (method == 0 || method == 1) {
+            if (i > 0) {
                 sum += diagMatrix.Diags[1][i - 1] * vector[i - 1];
 
                 if (i > diagMatrix.ZeroDiags + 1)
@@ -45,12 +46,10 @@ public record GaussSeidel(int MaxIters, double Eps, double W) : ISolver
             }
         }
 
-        if (method == 0 || method == 2)
-        {
+        if (method == 0 || method == 2) {
             sum += diagMatrix.Diags[0][i] * vector[i];
 
-            if (i < diagMatrix.Size - 1)
-            {
+            if (i < diagMatrix.Size - 1) {
                 sum += diagMatrix.Diags[3][i] * vector[i + 1];
 
                 if (i < diagMatrix.Size - diagMatrix.ZeroDiags - 2)
