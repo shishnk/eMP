5b13e86b8ab3d67fe0c8df2f7c8370cdeed3ed91
shishnk <akorund@mail.ru>
2022-04-28 23:52:13 +0700

    Добавил чекер для внутренних точек для произвольной сетки

--- eMP_1/Grid.cs
+++ eMP_1/Grid.cs
@@ -11,7 +11,7 @@ public abstract class Grid {
     public abstract void Build();
 
     public NormalType Normal(Point2D point) {
-        NormalType normalType = default(NormalType);
+        NormalType normalType = default;
 
         if (point.PointType != PointType.Boundary)
             throw new Exception("To determine the normal to the boundary, the point needs the boundary!");
@@ -39,41 +39,61 @@ public abstract class Grid {
 
     }
 
-    protected PointType PointsTypes(double x, double y) {
-        double eps = 1E-14;
+    // protected PointType PointsTypes(double x, double y) {
+    //     double eps = 1E-14;
 
-        if ((x > LinesX[0] && x < LinesX[2] && y > LinesY[0] && y < LinesY[1])
-        || (x > LinesX[0] && x < LinesX[1] && y > LinesY[0] && y < LinesY[2])
-        || (x == LinesX[1] && y == LinesY[1]))
-            return PointType.Internal;
+    //     if ((x > LinesX[0] && x < LinesX[2] && y > LinesY[0] && y < LinesY[1])
+    //     || (x > LinesX[0] && x < LinesX[1] && y > LinesY[0] && y < LinesY[2])
+    //     || (x == LinesX[1] && y == LinesY[1]))
+    //         return PointType.Internal;
 
-        for (int i = 0; i < AllLinesX.Count; i++)
-            if ((Math.Abs(x - AllLinesX[i]) < eps && Math.Abs(y - LinesY[0]) < eps)
-            || (Math.Abs(x - AllLinesX[i]) < eps && Math.Abs(y - LinesY[1]) < eps && x >= LinesX[1])
-            || (Math.Abs(x - AllLinesX[i]) < eps && Math.Abs(y - LinesY[2]) < eps && x <= LinesX[1]))
-                return PointType.Boundary;
+    //     for (int i = 0; i < AllLinesX.Count; i++)
+    //         if ((Math.Abs(x - AllLinesX[i]) < eps && Math.Abs(y - LinesY[0]) < eps)
+    //         || (Math.Abs(x - AllLinesX[i]) < eps && Math.Abs(y - LinesY[1]) < eps && x >= LinesX[1])
+    //         || (Math.Abs(x - AllLinesX[i]) < eps && Math.Abs(y - LinesY[2]) < eps && x <= LinesX[1]))
+    //             return PointType.Boundary;
 
-        for (int i = 0; i < AllLinesY.Count; i++)
-            if ((Math.Abs(y - AllLinesY[i]) < eps && Math.Abs(x - LinesX[0]) < eps)
-            || (Math.Abs(y - AllLinesY[i]) < eps && Math.Abs(x - LinesX[1]) < eps && y >= LinesY[1])
-            || (Math.Abs(y - AllLinesY[i]) < eps && Math.Abs(x - LinesX[2]) < eps && y <= LinesY[1]))
+    //     for (int i = 0; i < AllLinesY.Count; i++)
+    //         if ((Math.Abs(y - AllLinesY[i]) < eps && Math.Abs(x - LinesX[0]) < eps)
+    //         || (Math.Abs(y - AllLinesY[i]) < eps && Math.Abs(x - LinesX[1]) < eps && y >= LinesY[1])
+    //         || (Math.Abs(y - AllLinesY[i]) < eps && Math.Abs(x - LinesX[2]) < eps && y <= LinesY[1]))
+    //             return PointType.Boundary;
+
+    //     return PointType.Dummy;
+    // }
+
+    protected PointType PointTypesWithoutInternalCheck(double x, double y) {
+        foreach (var area in Areas)
+            if (x >= LinesX[area.Item4] && x <= LinesX[area.Item5] && y >= LinesY[area.Item6] && y <= LinesY[area.Item7])
                 return PointType.Boundary;
 
         return PointType.Dummy;
     }
 
+    protected void InternalCheck() {
+        foreach (var point in Points.Where(point => point.PointType != PointType.Dummy)) {
+            if (Points.Any(lstPoint => lstPoint.I == point.I - 1 && lstPoint.J == point.J && lstPoint.PointType != PointType.Dummy) &&
+                Points.Any(lstPoint => lstPoint.I == point.I + 1 && lstPoint.J == point.J && lstPoint.PointType != PointType.Dummy) &&
+                Points.Any(lstPoint => lstPoint.J == point.J - 1 && lstPoint.I == point.I && lstPoint.PointType != PointType.Dummy) &&
+                Points.Any(lstPoint => lstPoint.J == point.J + 1 && lstPoint.I == point.I && lstPoint.PointType != PointType.Dummy)) {
+                point.PointType = PointType.Internal;
+            }
+        }
+    }
+
     protected void SetAreaNumber() {
-        for (int i = 0; i < Points.Count; i++)
+        for (int i = 0; i < Points.Count; i++) {
             for (int iArea = 0; iArea < Areas.Length; iArea++) {
-                if (Points[i].X >= Areas[iArea].Item4 && Points[i].X <= Areas[iArea].Item5
-                && Points[i].Y >= Areas[iArea].Item6 && Points[i].Y <= Areas[iArea].Item7) {
+                if (Points[i].X >= Areas[iArea].Item4 && Points[i].X <= Areas[iArea].Item5 &&
+                    Points[i].Y >= Areas[iArea].Item6 && Points[i].Y <= Areas[iArea].Item7) {
                     Points[i].AreaNumber = Areas[iArea].Item1;
                 }
             }
+        }
     }
 
     public void AssignBoundaryConditions(Boundary[] boundaries) {
-        foreach (var point in Points.Where(point => point.PointType == PointType.Boundary))
+        foreach (var point in Points.Where(point => point.PointType == PointType.Boundary)) {
             for (int k = 0; k < boundaries.Length; k++) {
                 if (point.X >= LinesX[boundaries[k].X1] && point.X <= LinesX[boundaries[k].X2]
                 && point.Y >= LinesY[boundaries[k].Y1] && point.Y <= LinesY[boundaries[k].Y2]) {
@@ -81,6 +101,7 @@ public abstract class Grid {
                     break;
                 }
             }
+        }
     }
 
     protected void WriteToFilePoints() {
