d637ddf7c021a926366851919081249b60b82997
shishnk <akorund@mail.ru>
2022-05-03 20:26:07 +0700

    Фабрика для сеток

---
+++ eMP_3/Grids/GridFactory.cs
@@ -0,0 +1,22 @@
+namespace eMP_3;
+
+public enum GridTypes {
+    Regular,
+    Irregular
+}
+
+public interface IFactory {
+    public Grid CreateGrid(GridTypes gridType, GridParameters gridParameters);
+}
+
+public class GridFactory : IFactory {
+    public Grid CreateGrid(GridTypes gridType, GridParameters gridParameters) {
+        return gridType switch {
+            GridTypes.Regular => new RegularGrid(gridParameters),
+            
+            GridTypes.Irregular => new IrregularGrid(gridParameters),
+
+            _ => throw new InvalidEnumArgumentException($"This type of grid does not exist: {nameof(gridType)}")
+        };
+    }
+}
