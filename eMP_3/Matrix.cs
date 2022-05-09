9c6106d1f6555b22b7e94b07c65eeadb725308a8
shishnk <akorund@mail.ru>
2022-05-09 23:25:39 +0700

    Класс плотной матрицы

---
+++ eMP_3/Matrix.cs
@@ -0,0 +1,31 @@
+namespace eMP_3;
+
+public class Matrix {
+    private readonly double[,] storage;
+    public int Size { get; init; }
+
+    public double this[int i, int j] {
+        get => storage[i, j];
+        set => storage[i, j] = value;
+    }
+
+    public Matrix(int size) {
+        storage = new double[size, size];
+        Size = size;
+    }
+
+    public void Clear()
+        => Array.Clear(storage, 0, storage.Length);
+
+    public static Matrix operator +(Matrix fstMatrix, Matrix sndMatrix) {
+        Matrix resultMatrix = new(fstMatrix.Size);
+
+        for (int i = 0; i < resultMatrix.Size; i++) {
+            for (int j = 0; j < resultMatrix.Size; j++) {
+                resultMatrix[i, j] = fstMatrix[i, j] + sndMatrix[i, j];
+            }
+        }
+
+        return resultMatrix;
+    }
+}
