954c5371f6f1f84d716deea4212a3250838a49ae
shishnk <akorund@mail.ru>
2022-05-16 01:53:23 +0700

    Внес поправки

--- eMP_3/LinearBasis.cs
+++ eMP_3/LinearBasis.cs
@@ -1,27 +1,27 @@
 namespace eMP_3;
 
 public static class LinearBasis {
-    public static double Psi1(double x, double y, double z)
-        => (1 - x) * (1 - y) * (1 - z);
+    public static double Psi1(Point3D point)
+        => (1 - point.X) * (1 - point.Y) * (1 - point.Z);
 
-    public static double Psi2(double x, double y, double z)
-        => x * (1 - y) * (1 - z);
+    public static double Psi2(Point3D point)
+        => point.X * (1 - point.Y) * (1 - point.Z);
 
-    public static double Psi3(double x, double y, double z)
-        => y * (1 - x) * (1 - z);
+    public static double Psi3(Point3D point)
+        => point.Y * (1 - point.X) * (1 - point.Z);
 
-    public static double Psi4(double x, double y, double z)
-        => z * (1 - x) * (1 - y);
+    public static double Psi4(Point3D point)
+        => point.Z * (1 - point.X) * (1 - point.Y);
 
-    public static double Psi5(double x, double y, double z)
-        => (1 - x) * y * z;
+    public static double Psi5(Point3D point)
+        => (1 - point.X) * point.Y * point.Z;
 
-    public static double Psi6(double x, double y, double z)
-        => x * y * (1 - z);
+    public static double Psi6(Point3D point)
+        => point.X * point.Y * (1 - point.Z);
 
-    public static double Psi7(double x, double y, double z)
-        => x * z * (1 - y);
+    public static double Psi7(Point3D point)
+        => point.X * point.Z * (1 - point.Y);
 
-    public static double Psi8(double x, double y, double z)
-        => x * z * y;
+    public static double Psi8(Point3D point)
+        => point.X * point.Z * point.Y;
 }
