135d6c4b91a4a0dbca763a8fae4dfefdcc51a8da
shishnk <akorund@mail.ru>
2022-05-17 00:22:19 +0700

    Клиентский код

---
+++ eMP_3/Program.cs
@@ -0,0 +1,7 @@
+﻿using eMP_3;
+
+GridFactory gridFactory = new();
+var grid = gridFactory.CreateGrid(GridTypes.Regular, GridParameters.ReadJson("grid.jsonc")!.Value);
+
+FEM fem = FEM.CreateBuilder().SetSpaceGrid(grid).SetTest(new Test1()).SetSolverSLAE(new LOS(1000, 1E-14));
+fem.Compute();
