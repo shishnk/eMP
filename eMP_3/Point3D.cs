da2487711b28d9bab6e58f59519b844fb4c09e4f
shishnk <akorund@mail.ru>
2022-05-17 00:19:36 +0700

    Добавил перегрузки операторов

--- eMP_3/Point3D.cs
+++ eMP_3/Point3D.cs
@@ -1,6 +1,12 @@
 namespace eMP_3;
 
 public readonly record struct Point3D(double X, double Y, double Z) {
+    public static Point3D operator +(Point3D point, (double, double, double) value)
+        => new(point.X + value.Item1, point.Y + value.Item2, point.Z + value.Item3);
+
+    public static Point3D operator -(Point3D point, (double, double, double) value)
+        => new(point.X - value.Item1, point.Y - value.Item2, point.Z - value.Item3);
+
     public override string ToString()
         => $"{X} {Y} {Z}";
 }
