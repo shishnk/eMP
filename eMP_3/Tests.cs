cb57cb1d9749896f5a49f4fc68a7ecd4f44221a8
shishnk <akorund@mail.ru>
2022-05-23 00:43:27 +0700

    Подправил тест

--- eMP_3/Tests.cs
+++ eMP_3/Tests.cs
@@ -10,16 +10,16 @@ public interface ITest {
     public double Fc(Point3D point);
 }
 
-public class Test1 : ITest {
+public class Test1 : ITest { // все коэффициенты равны 1
     public double Fc(Point3D point)
-        => -point.X - point.Y - point.Z;
+       => -point.X + point.Y;
 
     public double Fs(Point3D point)
-        => (2 * point.X) + (2 * point.Y) + point.Z;
-
-    public double Uc(Point3D point)
-        => point.X + point.Y + point.Z;
+        => (-3 * point.X) - (3 * point.Y) - (2 * point.Z);
 
     public double Us(Point3D point)
-        => (2 * point.X) + (2 * point.Y) + point.Z;
+        => point.X + (2 * point.Y) + point.Z;
+
+    public double Uc(Point3D point)
+        => (2 * point.X) + point.Y + point.Z;
 }
