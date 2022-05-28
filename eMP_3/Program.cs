4fb998e5b657c068793b28cd9ed1732681f5a769
shishnk <akorund@mail.ru>
2022-05-28 13:41:27 +0700

    Клиентский код

--- eMP_3/Program.cs
+++ eMP_3/Program.cs
@@ -3,5 +3,12 @@
 GridFactory gridFactory = new();
 var grid = gridFactory.CreateGrid(GridTypes.Regular, GridParameters.ReadJson("grid.jsonc")!.Value);
 
-FEM fem = FEM.CreateBuilder().SetSpaceGrid(grid).SetTest(new Test1()).SetSolverSLAE(new LOS(1000, 1E-14));
-fem.Compute();
+#region Итерационный метод
+FEM fem1 = FEM.CreateBuilder().SetSpaceGrid(grid).SetTest(new Test1()).SetSolverSLAE(new BCGSTABLU(1000, 1E-14));
+fem1.Compute();
+#endregion
+
+#region Прямой метод
+FEM fem2 = FEM.CreateBuilder().SetSpaceGrid(grid).SetTest(new Test1()).SetDecomposer(new DecomposerLDU());
+fem2.Compute();
+#endregion
